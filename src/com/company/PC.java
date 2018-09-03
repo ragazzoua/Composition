00bb70b478b0b46266f5ef7b3dbbb612c2d19f3a
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-09-03 15:04:16 +0300

    create pc

---
+++ src/com/company/PC.java
@@ -0,0 +1,29 @@
+package com.company;
+
+/**
+ * created by s.a.miroshnychenko 9/3/2018
+ */
+
+public class PC {
+    private Case theCase;
+    private Monitor monitor;
+    private Motherboard motherboard;
+
+    public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
+        this.theCase = theCase;
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+    }
+
+    public Case getTheCase() {
+        return theCase;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public Motherboard getMotherboard() {
+        return motherboard;
+    }
+}
---
+++ src/com/company/Resolution.java
@@ -0,0 +1,23 @@
+package com.company;
+
+/**
+ * created by s.a.miroshnychenko 9/3/2018
+ */
+
+public class Resolution {
+    private int width;
+    private int height;
+
+    public Resolution(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
